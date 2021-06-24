bdb35dfd5156d9fabb82392fe872d5be18cb0f6f
murtinha <ericmurta@hotmail.com>
2021-06-24 11:09:59 -0300

    feat: add first model and db connection

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user:pass@localhost:5432/db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
